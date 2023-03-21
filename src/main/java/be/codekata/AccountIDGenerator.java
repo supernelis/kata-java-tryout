ccceb01374b307bace1f3303a162d4b290321d76
Saket Kulkarni <dev@cassk.nl>
2023-03-21 20:37:27 +0100

    Created AccountIDGenerator interface

---
+++ src/main/java/be/codekata/AccountIDGenerator.java
@@ -0,0 +1,5 @@
+package be.codekata;
+
+public interface AccountIDGenerator {
+    String generate();
+}
--- src/main/java/be/codekata/RandomAccountIDGenerator.java
+++ src/main/java/be/codekata/RandomAccountIDGenerator.java
@@ -2,8 +2,9 @@ package be.codekata;
 
 import java.util.UUID;
 
-public class RandomAccountIDGenerator {
+public class RandomAccountIDGenerator implements AccountIDGenerator {
 
+    @Override
     public String generate() {
         return UUID.randomUUID().toString();
     }
